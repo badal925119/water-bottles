4a84cea9d4259ed02fe151998cf81bff684cdd5e
Badal <badalhasanbd5@gmail.com>
2025-04-25 10:27:28 +0600

    add cart opiton added

--- src/components/Bottol/Bottol.jsx
+++ src/components/Bottol/Bottol.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Bottol.css'
 
-const Bottol = ({bottol}) => {
+const Bottol = ({bottol , addToCard}) => {
    const {name,img,price}=bottol
     return (
         <div className='bottol'> 
             <h2>Name : {name}</h2>
             <img src={img} alt="" />
             <p>Pirce : ${price}</p>
+            <button onClick={()=> addToCard(bottol)}>Add cart</button>
         </div>
     );
 };
--- src/components/Bottols/Bottols.jsx
+++ src/components/Bottols/Bottols.jsx
@@ -4,21 +4,29 @@ import './Bottols.css'
 
 const Bottols = () => {
     const [bottols, setBottols] = useState([]);
+    const [cart, setCart] = useState([]);
     
     useEffect(() => {
         fetch('bottols.json')
             .then(res => res.json())
             .then(data => setBottols(data));
     }, []);
+
+    const addToCard =bottol =>{
+        const newCart = [...cart, bottol];
+        setCart(newCart);
+    }
     
     return (
         <div>
             <h2>Total {bottols.length}</h2>
+            <h4>Cart : {cart.length}</h4>
          <div className='bottles'>
          {
             bottols.map(bottol => <Bottol 
                 key={bottol.id}
                 bottol={bottol}
+                addToCard={addToCard}
             ></Bottol>)
         }
          </div>
